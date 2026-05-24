6b50988db69ebec5d62ebe2fab5293d44c34bbfc
knightrishi <arnav.ara05@gmail.com>
2026-05-24 19:34:52 +0530

    made changes for as per mongodb needs

--- journalApp/src/main/java/net/arnavsingh/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/arnavsingh/journalApp/entity/JournalEntry.java
@@ -1,10 +1,30 @@
 package net.arnavsingh.journalApp.entity;
 
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
 //This is called a POJO class means "Plain Old Java Object"
+@Document(collection = "journal_entries")
 public class JournalEntry {
-    private long id;
+
+    @Id
+    private ObjectId id;
     private String title;
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    private LocalDateTime date;
 
     public String getContent() {
         return content;
@@ -22,11 +42,11 @@ public class JournalEntry {
         this.title = title;
     }
 
-    public long getId() {
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
