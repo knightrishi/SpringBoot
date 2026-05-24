2f9fda0e8344487ed6c298a99155f5ef7bc4db26
knightrishi <arnav.ara05@gmail.com>
2026-05-24 19:33:43 +0530

    Injected JournalEntryService into JournalEntryController

---
+++ journalApp/src/main/java/net/arnavsingh/journalApp/controller/JournalEntryControllerV2.java
@@ -0,0 +1,61 @@
+package net.arnavsingh.journalApp.controller;
+
+import net.arnavsingh.journalApp.entity.JournalEntry;
+import net.arnavsingh.journalApp.services.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+    return journalEntryService.getAll();
+
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry) {
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.save(myEntry);
+        return myEntry;
+
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId) {
+     return journalEntryService.findById(myId).orElse(null);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public boolean deleteJournalEntryById(@PathVariable ObjectId myId) {
+    journalEntryService.deleteById(myId);
+    return true;
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
+
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
+
+        if(old != null){
+        old.setTitle(newEntry.getTitle()!=null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+        old.setContent(newEntry.getContent()!=null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent() );
+        }
+
+        journalEntryService.save(old);
+        return old;
+    }
+}
