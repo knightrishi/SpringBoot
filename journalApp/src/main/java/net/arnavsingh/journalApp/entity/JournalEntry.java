79bdd11cbb464024d06624e60f663f573501bc3d
knightrishi <arnav.ara05@gmail.com>
2026-05-26 01:13:53 +0530

    @Getter and @Setter generated using lombok

--- journalApp/src/main/java/net/arnavsingh/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/arnavsingh/journalApp/entity/JournalEntry.java
@@ -1,54 +1,26 @@
 package net.arnavsingh.journalApp.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+
 
 //This is called a POJO class means "Plain Old Java Object"
 @Document(collection = "journal_entries")
+
+@Getter @Setter
 public class JournalEntry {
 
     @Id
     private ObjectId id;
     private String title;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
     private LocalDateTime date;
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
     private String content;
 
