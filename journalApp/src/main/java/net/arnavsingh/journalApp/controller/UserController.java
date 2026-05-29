0ef7f535e32b32502858af299305089a9768ebe2
knightrishi <arnav.ara05@gmail.com>
2026-05-29 23:09:06 +0530

    More detailed api created on per-user basis

--- journalApp/src/main/java/net/arnavsingh/journalApp/controller/UserController.java
+++ journalApp/src/main/java/net/arnavsingh/journalApp/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<User> getAll(){
+    public List<User> getAllJournalEntriesOfUser(){
         return userService.getAll();
     }
 
