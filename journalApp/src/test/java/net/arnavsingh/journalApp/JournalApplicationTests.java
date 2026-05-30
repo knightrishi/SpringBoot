4420ba7cbc1e770a8c2e901f1f9912530985520b
knightrishi <arnav.ara05@gmail.com>
2026-05-30 23:47:15 +0530

    Test

---
+++ journalApp/src/test/java/net/arnavsingh/journalApp/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package net.arnavsingh.journalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
